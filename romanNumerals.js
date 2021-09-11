4088b4923c8d70750bd01556d838a73f1bd69ee9
Lucas Ariel Peñalva <lucasarielpenalva@gmail.com>
2021-09-11 20:13:03 -0300

    Update romanNumerals.js
    Made the function clearer and applied formatting with Prettier.

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,12 +1,25 @@
 class RomanNumerals {
-  
-  static roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+  static roman = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
   static decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
   static toRoman(num) {
-
-    if(typeof num !== "number") throw new TypeError("The 'toRoman' method only accepts numbers.");
-    if(num < 1) return new RangeError("The number must be greater than 0.");
+    if (typeof num !== "number")
+      throw new TypeError("The 'toRoman' method only accepts numbers.");
+    if (num < 1) return new RangeError("The number must be greater than 0.");
 
     let encoded = "";
 
@@ -14,37 +27,33 @@ class RomanNumerals {
       while (this.decimal[i] <= num) {
         num -= this.decimal[i];
         encoded += this.roman[i];
-      };
+      }
     });
 
     return encoded;
-  };
+  }
 
   static fromRoman(str) {
-    if(typeof str !== "string") throw new TypeError("The 'fromRoman' method only accepts strings.");
+    if (typeof str !== "string")
+      throw new TypeError("The 'fromRoman' method only accepts strings.");
 
     let num = 0;
 
     for (let i = 0; i < str.length; i++) {
+      const checkTwoLetters = str[i] + str[i + 1];
 
-      let twoLetters = str[i] + str[i + 1];
-      let twoIndex = this.roman.indexOf(twoLetters);
-
-      if (twoIndex !== -1) {
-        
-        num += this.decimal[twoIndex];
+      if (this.roman.includes(checkTwoLetters)) {
+        num += this.decimal[this.roman.indexOf(checkTwoLetters)];
         i++;
-
       } else {
+        const romanToNumber = this.decimal[this.roman.indexOf(str[i])];
+        if (romanToNumber === undefined)
+          throw new RangeError("Invalid letters in string.");
 
-        let romanToNumber = this.decimal[this.roman.indexOf(str[i])];
-        if(romanToNumber === undefined) throw new RangeError("Invalid letters in string.");
-       
         num += romanToNumber;
-      };
-    };
-    
-    return num;
-  };
+      }
+    }
 
-};
+    return num;
+  }
+}
